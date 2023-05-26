e9a8100f0ab026232ff43d67d694a17fca54dfb1
박찬준 <pcj01142@gmail.com>
2023-05-26 16:51:19 +0900

    AlarmDao 작성

---
+++ app/src/main/java/com/jingom/simplealarmmanager/data/alarm/dao/AlarmDao.kt
@@ -0,0 +1,20 @@
+package com.jingom.simplealarmmanager.data.alarm.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import com.jingom.simplealarmmanager.domain.model.alarm.Alarm
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface AlarmDao {
+	@Insert
+	fun insert(alarm: Alarm)
+
+	@Delete
+	fun delete(alarm: Alarm)
+
+	@Query("SELECT * FROM alarm")
+	fun getAllFlow(): Flow<Alarm>
+}
