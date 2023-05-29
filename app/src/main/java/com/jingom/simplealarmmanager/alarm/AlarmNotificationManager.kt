45e6a0c49425b15349b98fb5cc584746914a878e
박찬준 <pcj01142@gmail.com>
2023-05-29 10:46:27 +0900

    알람 내용 수정

--- app/src/main/java/com/jingom/simplealarmmanager/alarm/AlarmNotificationManager.kt
+++ app/src/main/java/com/jingom/simplealarmmanager/alarm/AlarmNotificationManager.kt
@@ -6,6 +6,8 @@ import android.graphics.BitmapFactory
 import android.util.Log
 import androidx.core.app.NotificationCompat
 import com.jingom.simplealarmmanager.R
+import com.jingom.simplealarmmanager.common.date.DateTimeFormatters
+import com.jingom.simplealarmmanager.common.date.formatWithLocale
 import com.jingom.simplealarmmanager.common.notification.NotificationChannelType
 import com.jingom.simplealarmmanager.domain.model.alarm.Alarm
 import dagger.hilt.android.qualifiers.ApplicationContext
@@ -27,11 +29,12 @@ class DefaultAlarmNotificationManager(
 		val largeIcon = BitmapFactory.decodeResource(applicationContext.resources, R.drawable.ic_launcher_foreground)
 		val appChannelId = NotificationChannelType.APP_ALARM.id
 
-		val content = alarm.name
+		val title = alarm.name
+		val content = alarm.time.formatWithLocale(DateTimeFormatters.ALARM_TIME_AM_PM)
 
 		val notification = NotificationCompat.Builder(applicationContext, appChannelId)
 			.setTicker(content)
-			.setContentTitle(applicationContext.resources.getString(R.string.app_name))
+			.setContentTitle(title)
 			.setContentText(content)
 			.setWhen(System.currentTimeMillis())
 			.setLargeIcon(largeIcon)
