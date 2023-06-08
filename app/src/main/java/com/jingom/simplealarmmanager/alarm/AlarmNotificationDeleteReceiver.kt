1c5f47d0c30658fdd4f315c2fab0978bf241ff5c
박찬준 <pcj01142@gmail.com>
2023-06-08 22:27:08 +0900

    앱 알람 울리는 경우 음악이 재생되는 기능 추가

---
+++ app/src/main/java/com/jingom/simplealarmmanager/alarm/AlarmNotificationDeleteReceiver.kt
@@ -0,0 +1,22 @@
+package com.jingom.simplealarmmanager.alarm
+
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+
+class AlarmNotificationDeleteReceiver: BroadcastReceiver() {
+
+	override fun onReceive(context: Context?, intent: Intent?) {
+		context ?: return
+		intent ?: return
+
+		val alarmInfo = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+			intent.getParcelableExtra(AlarmInfo.KEY, AlarmInfo::class.java)
+		} else {
+			intent.getParcelableExtra(AlarmInfo.KEY) as? AlarmInfo
+		} ?: return
+
+		AlarmNotifyWorker.cancelWork(alarmInfo.id, context)
+	}
+}
--- app/src/main/java/com/jingom/simplealarmmanager/alarm/AlarmNotificationManager.kt
+++ app/src/main/java/com/jingom/simplealarmmanager/alarm/AlarmNotificationManager.kt
@@ -1,7 +1,9 @@
 package com.jingom.simplealarmmanager.alarm
 
 import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
+import android.content.Intent
 import android.graphics.BitmapFactory
 import android.provider.Settings
 import android.util.Log
@@ -31,7 +33,7 @@ class DefaultAlarmNotificationManager(
 		val appChannelId = NotificationChannelType.APP_ALARM.id
 
 		val title = alarm.name
-		val content = alarm.time.formatWithLocale(DateTimeFormatters.ALARM_TIME_AM_PM)
+		val content = getAlarmContent(alarm)
 
 		val notification = NotificationCompat.Builder(applicationContext, appChannelId)
 			.setTicker(content)
@@ -44,8 +46,37 @@ class DefaultAlarmNotificationManager(
 			.setVibrate(longArrayOf(1000, 500, 500, 1000, 1000))
 			.setOnlyAlertOnce(true)
 			.setAutoCancel(true)
+			.setDeleteIntent(getNotificationDeletePendingIntent(alarm))
 			.build()
 
 		notificationManager.notify(alarm.id.toInt(), notification)
+	}
+
+	private fun getAlarmContent(alarm: Alarm): String {
+		val alarmTimeString = alarm.time.formatWithLocale(DateTimeFormatters.ALARM_TIME_AM_PM)
+		val cancelString = applicationContext.getString(R.string.swipe_to_cancel_alarm)
+
+		return "$alarmTimeString $cancelString"
+	}
+
+	private fun getNotificationDeletePendingIntent(alarm: Alarm): PendingIntent {
+		val pendingIntentFlag = PendingIntent.FLAG_ONE_SHOT or PendingIntent.FLAG_IMMUTABLE
+
+		return PendingIntent.getBroadcast(
+			applicationContext,
+			getAlarmRequestCode(alarm),
+			getAlarmIntent(alarm),
+			pendingIntentFlag
+		)
+	}
+
+	private fun getAlarmRequestCode(alarm: Alarm): Int {
+		return alarm.id.toInt()
+	}
+
+	private fun getAlarmIntent(alarm: Alarm): Intent {
+		return Intent(applicationContext, AlarmNotificationDeleteReceiver::class.java).apply {
+			putExtra(AlarmInfo.KEY, alarm.toIntentModel())
+		}
 	}
 }
